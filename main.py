c4c845b414189c10bd8527d3fed65e37e5ec093c
Kanael-dev <kanael.dev@gmail.com>
2025-11-16 16:56:57 -0400

    Ajuste na porta

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+import os
 from flask import Flask, request
 from router.routers import players_router
 from router.authenticate import generate_token
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 
-CORS(app, origins="*",
-     methods=["GET", "POST", "PUT", "DELETE"],
-     allow_headers=["Content-Type", "Authorization"])
-
+CORS(
+    app,
+    origins="*",
+    methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Content-Type", "Authorization"],
+)
 
 # Registra o Blueprint
 app.register_blueprint(players_router)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False 
+    )
